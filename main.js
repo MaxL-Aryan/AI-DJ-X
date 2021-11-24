3882407dd24b610e5fe2c4bda21d6ec08842a1d2
MaxL-Aryan <79088145+MaxL-Aryan@users.noreply.github.com>
2021-11-24 17:46:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
+song="";
+
+function preload()
+{
+    song =loadSong("music.mp3");
+}
+
 function setup()
 {
-    canvas = createCanvas(520,450);
+    canvas = createCanvas(600,450);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video,0,0,600,450);
+}
+
+function play()
+{
+    song.play();
 }
